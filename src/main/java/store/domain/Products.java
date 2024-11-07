585f62985dfb3b1cd3cb1ff3a79b0d52dc774190
donghyunn1 <dongdong0516@naver.com>
2024-11-07 21:25:13 +0900

    feat: 파일로부터 products에 리스트로 생성 기능 구현

---
+++ src/main/java/store/domain/Products.java
@@ -0,0 +1,17 @@
+package store.domain;
+
+import java.util.List;
+
+public class Products {
+
+
+    private static List<Product> products = ProductFileReader.readProducts();
+
+    public void displayProducts() {
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+
+}
