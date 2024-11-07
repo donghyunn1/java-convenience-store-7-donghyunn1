5926fe471d7f57a6fcad458aaf6abcb886515356
donghyunn1 <dongdong0516@naver.com>
2024-11-08 03:06:07 +0900

    fix: 프로모션 적용 여부를 boolean형태로 반환

--- src/main/java/store/domain/PromotionService.java
+++ src/main/java/store/domain/PromotionService.java
@@ -1,23 +1,24 @@
 package store.domain;
 
 
+import camp.nextstep.edu.missionutils.Console;
 
 public class PromotionService {
 
-    public void applyPromotion(Product product, int purchaseQuantity) {
-
-        if (product.getPromotion() != null) {
-            String promotionName = product.getPromotion();
-
-            // 탄산 2+1 프로모션 적용
-            if (promotionName.equals("탄산2+1") && (purchaseQuantity == 2)) {
-                System.out.println("현재 " + product.getName() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-            }
-
-            // MD 추천상품, 반짝할인 1+1 프로모션 적용
-            if ((promotionName.equals("MD추천상품") || promotionName.equals("반짝할인")) && (purchaseQuantity == 1)) {
-                System.out.println("현재 " + product.getName() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
-            }
+    public boolean applyPromotion(Product product, int purchaseQuantity) {
+        String promotionName = product.getPromotion();
+        if (promotionName.equals("탄산2+1") && (purchaseQuantity == 2)) {
+            return askBonus(product);
         }
+        if ((promotionName.equals("MD추천상품") || promotionName.equals("반짝할인")) && (purchaseQuantity == 1)) {
+            return askBonus(product);
+        }
+        return false;
+    }
+
+    private boolean askBonus(Product product) {
+        System.out.println("현재 " + product.getName() + "은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        String userInput = Console.readLine();
+        return userInput.equals("Y");
     }
 }
