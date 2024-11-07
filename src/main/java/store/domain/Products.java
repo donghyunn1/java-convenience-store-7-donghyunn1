64d1955f91ee19e493730c907587c078aee7b777
donghyunn1 <dongdong0516@naver.com>
2024-11-07 22:09:34 +0900

    feat: 구입한 상품명과 매칭되는 품목을 찾는 기능 구현

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,5 +1,6 @@
 package store.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Products {
@@ -10,5 +11,21 @@ public class Products {
         for (Product product : products) {
             System.out.println(product);
         }
+    }
+
+    public Product findProductByName(String productName) {
+        List<Product> matchingProducts = new ArrayList<>();
+        for (Product product : products) {
+            if (product.getName().equals(productName)) {
+                matchingProducts.add(product);
+            }
+        }
+        for (Product product : matchingProducts) {
+            if (product.getQuantity() > 0) {
+                return product;
+            }
+        }
+        return null;
+
     }
 }
