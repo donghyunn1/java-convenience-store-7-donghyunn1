d30f2f9bc73bd3fafd525df528644e0b7c80cd5c
donghyunn1 <dongdong0516@naver.com>
2024-11-09 03:51:45 +0900

    feat: 영수증 정보 출력 구현

--- src/main/java/store/domain/receipt/Receipt.java
+++ src/main/java/store/domain/receipt/Receipt.java
@@ -48,4 +48,42 @@ public class Receipt {
     private int getFinalAmount() {
         return getTotalAmount()  - promotionDiscount;
     }
+
+    public void print() {
+        printHeader();
+        printItems();
+        printFreeItems();
+        printDivider();
+        printSummary();
+    }
+
+    private void printHeader() {
+        System.out.println("==============W 편의점================");
+        System.out.println("상품명\t\t\t수량\t\t금액");
+    }
+
+    private void printItems() {
+        for (Map.Entry<Product, Integer> entry : items.entrySet()) {
+            Product product = entry.getKey();
+            int quantity = entry.getValue();
+            System.out.printf("%s\t\t\t%d\t\t%,d\n", product.getName(), quantity, product.getPrice() * quantity);
+        }
+    }
+
+    private void printFreeItems() {
+        System.out.println("=============증     정===============");
+        for (Map.Entry<String, Integer> entry : freeItems.entrySet()) {
+            System.out.printf("%s\t\t\t%d\n", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void printDivider() {
+        System.out.println("====================================");
+    }
+
+    private void printSummary() {
+        System.out.printf("총구매액\t\t\t%d %,d\n", getTotalQuantity(), getTotalAmount());
+        System.out.printf("행사할인\t\t\t-%,d\n", promotionDiscount);
+        System.out.printf("내실돈\t\t\t\t\t%,d\n", getFinalAmount());
+    }
 }
