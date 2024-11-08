2f82a1373769592ea4bf9e2dc133800053277cdf
donghyunn1 <dongdong0516@naver.com>
2024-11-09 03:31:55 +0900

    feat: 주문을 입력받고 주문이 완료되면 종료하는 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,13 +1,44 @@
 package store;
 
+import camp.nextstep.edu.missionutils.Console;
+import store.domain.order.OrderParser;
 import store.domain.store.Store;
+
+import java.util.Map;
 
 
 public class Application {
     public static void main(String[] args) {
+        try {
+            Store store = new Store();
+            boolean continueOrder = true;
 
-        Store store = new Store();
+            while(continueOrder) {
+                processOrder(store);
+                continueOrder = askMoreOrders();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-        store.displayProducts();
+    private static void processOrder(Store store) {
+        try {
+            store.displayProducts();
+            String orderInput = receiveOrder();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String receiveOrder() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        return Console.readLine();
+    }
+
+    private static boolean askMoreOrders() {
+        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+        String answer = Console.readLine();
+        return answer.equalsIgnoreCase("y");
     }
 }
