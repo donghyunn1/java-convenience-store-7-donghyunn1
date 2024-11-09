e6655957c6dd1d5f94538e30827e316e27b73a4e
donghyunn1 <dongdong0516@naver.com>
2024-11-10 03:57:11 +0900

    refactor: controller 도메인으로 분리

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,6 +1,7 @@
 package store;
 
 import camp.nextstep.edu.missionutils.Console;
+import store.controller.StoreController;
 import store.domain.order.OrderParser;
 import store.domain.receipt.Receipt;
 import store.domain.store.Store;
@@ -10,56 +11,8 @@ import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            Store store = new Store();
-            boolean continueOrder = true;
 
-            while(continueOrder) {
-                processOrder(store);
-                continueOrder = askMoreOrders();
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void processOrder(Store store) {
-        try {
-            store.displayProducts();
-            String orderInput = receiveOrder();
-            Map<String, Integer> orderItems = OrderParser.parse(orderInput);
-
-            Map<String, Integer> finalOrderItems = store.promotionAddSuggestion(orderItems);
-
-            boolean isMemberShip = useMembership();
-
-            Receipt receipt = store.processOrder(finalOrderItems, isMemberShip);
-            receipt.print();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static boolean useMembership() {
-        while (true) {
-            try {
-                System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-                return Console.readLine().equals("Y");
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
-            }
-        }
-    }
-
-    private static boolean askMoreOrders() {
-        while (true) {
-            try {
-                System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-                return Console.readLine().equals("Y");
-            } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
-            }
-        }
-
+        StoreController storeController = new StoreController();
+        storeController.run();
     }
 }
---
+++ src/main/java/store/controller/StoreController.java
@@ -0,0 +1,41 @@
+package store.controller;
+
+import store.domain.order.OrderParser;
+import store.domain.receipt.Receipt;
+import store.domain.store.Store;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.util.Map;
+
+public class StoreController {
+    private final Store store;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public StoreController() {
+        this.store = new Store();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run() {
+        boolean continueOrder = true;
+        while (continueOrder) {
+            processOrder();
+            continueOrder = inputView.inputMoreOrders();
+        }
+    }
+
+    private void processOrder() {
+        store.displayProducts();
+        Map<String, Integer> orderItems = OrderParser.parse(inputView.inputOrder());
+        orderItems = store.promotionAddSuggestion(orderItems);
+
+        boolean useMembership = inputView.inputMembership();
+
+        Receipt receipt = store.processOrder(orderItems, useMembership);
+        receipt.print();
+    }
+}
+
