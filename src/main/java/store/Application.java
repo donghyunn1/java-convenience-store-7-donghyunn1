242a75dc78260db92f490a9bcc94ef7bc8b4e4b1
donghyunn1 <dongdong0516@naver.com>
2024-11-10 01:51:34 +0900

    feat: 잘못된 입력을 받았을 때 다시 입력받을 수 있는 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -41,18 +41,36 @@ public class Application {
     }
 
     private static boolean useMembership() {
-        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
-        return Console.readLine().equals("Y");
+        while (true) {
+            try {
+                System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+                return Console.readLine().equals("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
     }
 
     private static String receiveOrder() {
-        System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+                return Console.readLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
     }
 
     private static boolean askMoreOrders() {
-        System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
-        String answer = Console.readLine();
-        return answer.equalsIgnoreCase("y");
+        while (true) {
+            try {
+                System.out.println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)");
+                return Console.readLine().equals("Y");
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+            }
+        }
+
     }
 }
